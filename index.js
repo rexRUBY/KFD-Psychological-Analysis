a6bf7fda4286e6272a450dc09da57b816b05203b
YeseoPark <YeseoPark>
2023-10-16 00:25:59 +0900

    231015

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const database = require('mysql2');
 const readXlsxFile = require('read-excel-file/node');
 
 const app = express();
@@ -15,14 +15,14 @@ app.use(bodyParser.json()); // JSON 데이터를 파싱
 app.use(bodyParser.urlencoded({ extended: false })); // URL-encoded 데이터를 파싱
 
 // DB 연결
-var connection = mysql.createConnection({
+var connection = database.createConnection({
     host: 'svc.sel5.cloudtype.app',
-    port: 30942,
+    port: 30376,
     user: 'root',
     password: 'dlsrhdwlsmd00!',
     database: 'kfd_db'
-});
-
+  });
+//svc.sel5.cloudtype.app:30376
 // DB 연결 확인
 connection.connect(function (err) {
     if (err) {
@@ -58,38 +58,32 @@ readXlsxFile("./label.xlsx").then((rows) => {
     }
 });
 
-
 // index.html에서 받은 데이터 DB에서 검색
 app.post('/search', function (req, res) {
     var length = req.body.length;
     var values = req.body;
-    console.log('검색어: ' + values);
-
+    
     // 검색된 모든 이미지 URL을 저장할 배열
     var allImageURLs = [];
 
-    // 모든 검색어에 대한 조건을 AND 연산으로 조합
-    var conditions = [];
     for (var i = 0; i < length; i++) {
-        conditions.push('tags LIKE ?');
+        var sql = 'SELECT image_url FROM images WHERE tags LIKE ?';
+        var value = ['%' + values[i] + '%'];
+        connection.query(sql, value, function (error, results) {
+            if (error) {
+                console.error('SQL error: ' + error.message);
+                return;
+            }
+            // 한 번의 쿼리 결과를 allImageURLs 배열에 추가
+            allImageURLs.push(results);
+
+            // 모든 쿼리가 완료되면 한 번에 응답을 보냅니다.
+            if (allImageURLs.length === length) {
+                res.send(allImageURLs);
+                console.log('검색 결과: ' + JSON.stringify(allImageURLs));
+            }
+        });
     }
-
-    // 모든 조건을 AND 연산으로 결합하여 쿼리 생성
-    var sql = 'SELECT image_url FROM images WHERE ' + conditions.join(' AND ');
-
-    // 각 검색어에 대한 값 배열 구성
-    var valuesArray = values.map(function (value) {
-        return '%' + value + '%';
-    });
-
-    connection.query(sql, valuesArray, function (error, results) {
-        if (error) {
-            console.error('SQL error: ' + error.message);
-            return;
-        }
-        res.send(results);
-        console.log('검색 결과: ' + JSON.stringify(results));
-    });
 });
 
 
