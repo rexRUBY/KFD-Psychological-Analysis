78d15391b831dce0b9b66203108b433f99d7d9ef
yeseo <ye_2002@naver.com>
2023-11-04 16:24:02 +0900

    231104

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='keybert',
+    version='0.0.1',
+    description='BERT for Keyword Extraction',
+    author='MaartenGr',
+    url='https://github.com/MaartenGr/KeyBERT.git',
+    packages=['keybert'],
+)
